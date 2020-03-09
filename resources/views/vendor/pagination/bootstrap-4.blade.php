6973d32ae9a5b785e067265a5f1337c2349180bf
Giannos Sazos <32360330+GiannosSazos@users.noreply.github.com>
2020-03-09 22:22:30 +0000

    Design Fixes
    Implemented the bulma design for pagination and fixed the background for the users/index and main index because background colour would not apply to entire page

--- resources/views/vendor/pagination/bootstrap-4.blade.php
+++ resources/views/vendor/pagination/bootstrap-4.blade.php
@@ -1,46 +1,42 @@
 @if ($paginator->hasPages())
+    <nav class="pagination is-centered" role="navigation" aria-label="pagination">
+        {{-- Previous Page Link --}}
+        @if ($paginator->onFirstPage())
+            <a class="pagination-previous" disabled>&laquo;</a>
+        @else
+            <a class="pagination-previous" href="{{ $paginator->previousPageUrl() }}">&laquo;</a>
+        @endif
 
-        <ul class="pagination">
-            {{-- Previous Page Link --}}
-            @if ($paginator->onFirstPage())
-                <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.previous')">
-                    <span class="page-link" aria-hidden="true">&lsaquo;</span>
-                </li>
-            @else
-                <li class="page-item">
-                    <a class="page-link" href="{{ $paginator->previousPageUrl() }}" rel="prev" aria-label="@lang('pagination.previous')">&lsaquo;</a>
-                </li>
-            @endif
+        {{-- Next Page Link --}}
+        @if ($paginator->hasMorePages())
+            <a class="pagination-next" href="{{ $paginator->nextPageUrl() }}"> &raquo;</a>
+        @else
+            <a class="pagination-next" disabled>&raquo;</a>
+        @endif
 
-            {{-- Pagination Elements --}}
+
+        {{-- Pagination Elements --}}
+        <ul class="pagination-list">
             @foreach ($elements as $element)
                 {{-- "Three Dots" Separator --}}
                 @if (is_string($element))
-                    <li class="page-item disabled" aria-disabled="true"><span class="page-link">{{ $element }}</span></li>
+                    <li><span class="pagination-ellipsis">&hellip;</span></li>
                 @endif
 
                 {{-- Array Of Links --}}
                 @if (is_array($element))
                     @foreach ($element as $page => $url)
                         @if ($page == $paginator->currentPage())
-                            <li class="page-item active" aria-current="page"><span class="page-link">{{ $page }}</span></li>
+                            <li><a class="pagination-link is-current" aria-label="Goto page {{ $page }}">{{ $page }}</a>
+                            </li>
                         @else
-                            <li class="page-item"><a class="page-link" href="{{ $url }}">{{ $page }}</a></li>
+                            <li><a href="{{ $url }}" class="pagination-link"
+                                   aria-label="Goto page {{ $page }}">{{ $page }}</a></li>
                         @endif
                     @endforeach
                 @endif
             @endforeach
-
-            {{-- Next Page Link --}}
-            @if ($paginator->hasMorePages())
-                <li class="page-item">
-                    <a class="page-link" href="{{ $paginator->nextPageUrl() }}" rel="next" aria-label="@lang('pagination.next')">&rsaquo;</a>
-                </li>
-            @else
-                <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.next')">
-                    <span class="page-link" aria-hidden="true">&rsaquo;</span>
-                </li>
-            @endif
         </ul>
 
+    </nav>
 @endif
